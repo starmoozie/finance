1b0139ddcb4f8e80573e32bf68c520a22e9de1c2
Bagus Kurniawan <baguskurniawan@192.168.1.200>
2023-08-20 21:49:53 +0700

    add model db product

---
+++ database/migrations/2023_08_20_214323_create_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('created_by', 14)->index();
+            $table->string('name', 30);
+            $table->string('stock', 15)->nullable();
+            $table->string('price', 15)->nullable();
+            $table->json('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
