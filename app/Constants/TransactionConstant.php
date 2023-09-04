b4b26f0b80970ae443729d5df69ac61902f6a3f1
Bagus Kurniawan <bagusyuli@impstudio.co.id>
2023-09-04 20:16:02 +0700

    change type transaction

--- app/Constants/TransactionConstant.php
+++ app/Constants/TransactionConstant.php
@@ -5,12 +5,12 @@ namespace App\Constants;
 class TransactionConstant
 {
     const SALE     = 0;
-    const EXPENSE  = 1;
-    const PURCHASE = 2;
+    const PURCHASE = 1;
+    const EXPENSE  = 2;
 
     const ALL      = [
         ['label' => 'sale', 'value' => 0, 'color' => 'success'],
-        ['label' => 'expense', 'value' => 1, 'color' => 'warning'],
-        ['label' => 'purchase', 'value' => 2, 'color' => 'danger'],
+        ['label' => 'purchase', 'value' => 1, 'color' => 'warning'],
+        ['label' => 'expense', 'value' => 2, 'color' => 'danger'],
     ];
 }
